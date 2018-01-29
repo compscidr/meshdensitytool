2912f477bfbdc2e32e894d204d49991cd8f1e374
Ben Hughes <benjamin.b.hughes@gmail.com>
2018-01-28 20:08:55 -0800

    Create EnergyLink class for easy link creation
    Energy links have no delay and no cost

--- js/density.js
+++ js/density.js
@@ -30,6 +30,12 @@ class Link {
   }
 }
 
+class EnergyLink extends Link{
+  constructor(left, right, type, energy) {
+    super(left, right, type, 0, energy, 0)
+  }
+}
+
 /**
  * The simulator engine.
  */
