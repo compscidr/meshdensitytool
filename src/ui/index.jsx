905a2f84eca8746c3b81a53cda40df6216204f31
Ben Hughes <benjamin.b.hughes@gmail.com>
2018-02-01 17:22:10 -0800

    Inputs are now React components

--- src/ui/index.jsx
+++ src/ui/index.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+class SimulationParameter extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {value: this.props.default }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value})
+  }
+  render() {
+    return (
+      <div className="SimulationParameter">
+        <label>
+        {this.props.label}
+        <br />
+        <input id={this.props.id} type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <br />
+      </div>
+    )
+  }
+}
+
 class App extends React.Component {
   render () {
-    return <p> Webpack is watching React! </p>
+    return (
+      <div>
+      <SimulationParameter label="Population density / square km:" id="density" default="100" />
+      <SimulationParameter label="Wifi hotspot percentage:" id="ap" default="10" />
+      <SimulationParameter label="Wifi hotspot range:" id="coverage" default="20" />
+      <SimulationParameter label="Wifi-direct hotspot percentage:" id="dap" default="5" />
+      </div>
+    )
   }
 }
 
