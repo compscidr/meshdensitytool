1af0ff42f45fca2a5ce57eafec718224c864dfe1
Ben Hughes <benjamin.b.hughes@gmail.com>
2018-02-04 21:59:58 -0800

    Convert to a react app
    Swap out the base rendering system from html with injected react to
    react with injected html.

--- src/AppUI.js
+++ src/AppUI.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { Component } from 'react';
+
 import ReactDOM from 'react-dom'
 
 class SimulationParameter extends React.Component {
@@ -38,38 +39,4 @@ class AppUI extends React.Component {
   }
 }
 
-class StatOutput extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render () {
-    return (
-      <div className="StatOutput">
-        <div>{this.props.id}</div>
-        <div id={this.props.id}></div>
-      </div>
-    )
-  }
-}
-
-class AppStats extends React.Component {
-  render () {
-    return (
-      <div>
-        <StatOutput id="stat-density" />
-        <StatOutput id="stat-wifi-hotspot-percent" />
-        <StatOutput id="stat-wifi-hotspot-range" />
-        <StatOutput id="stat-wifi-hotspot-coverage" />
-        <StatOutput id="stat-wifi-average-hotspots" />
-        <StatOutput id="stat-wifi-average-clients" />
-        <StatOutput id="stat-total-energy" />
-        <StatOutput id="stat-unconnected" />
-        <StatOutput id="stat-largest-local" />
-      </div>
-    )
-  }
-}
-
-ReactDOM.render(<AppUI />, document.getElementById('app-ui'))
-ReactDOM.render(<AppStats />, document.getElementById('app-stats'))
+export default AppUI;
