3fcd43fe3c4d6a8131e5b9ea15be6602b5f3f445
Ben Hughes <benjamin.b.hughes@gmail.com>
2018-02-09 14:51:17 -0800

    Remove serviceWorker and set doc root

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import AppUI from './AppUI';
 import AppStats from './AppStats';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<AppUI />, document.getElementById('app-ui'))
 ReactDOM.render(<AppStats />, document.getElementById('app-stats'))
-registerServiceWorker();
