b67f07d393a291f0dac64ea397fb978c2f61168e
Ben Hughes <benjamin.b.hughes@gmail.com>
2018-02-10 15:55:44 -0800

    Remove refs to global intervalid

--- public/regions.js
+++ public/regions.js
@@ -1,23 +1,19 @@
 // Characteristicis of Canada
 function canada () {
     $('#density').val('4')
-    if (intervalid !== -1) { clearInterval(intervalid) }
 }
 
 // Characteristicis of Guatamala City
 function guatcity () {
     $('#density').val('1000')
-    if (intervalid !== -1) { clearInterval(intervalid) }
 }
 
 // Characteristicis of Toronto
 function tor () {
     $('#density').val('2650')
-    if (intervalid !== -1) { clearInterval(intervalid) }
 }
 
 // Characteristic=is of Vancouver
 function van () {
     $('#density').val('5249')
-    if (intervalid !== -1) { clearInterval(intervalid) }
 }
