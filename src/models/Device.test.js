07584b58ab7ccc942cade2176711dccf7fb235d5
Ben Hughes <benjamin.b.hughes@gmail.com>
2018-02-13 11:55:52 -0800

    Add limited device tests

--- src/models/Device.test.js
+++ src/models/Device.test.js
@@ -1,5 +1,30 @@
 import Device, { CLAMP_BOUNCE } from './Device'
 
-it('is cool', () => {
-  const device = new Device(10, 10, CLAMP_BOUNCE)
+describe('A mesh device', () => {
+  test('can be created at a position with clamping', () => {
+    const device = new Device(10, 11, CLAMP_BOUNCE)
+    expect(device.x).toBe(10)
+    expect(device.y).toBe(11)
+    expect(device.dx).toBe(0)
+    expect(device.dy).toBe(0)
+    expect(device.clamp).toBe(CLAMP_BOUNCE)
+  })
+  test('can be moved by hand', () => {
+    const device = new Device(5, 6, CLAMP_BOUNCE)
+    expect(device.x).toBe(5)
+    expect(device.y).toBe(6)
+    device.x = 10
+    expect(device.x).toBe(10)
+    device.y = 11
+    expect(device.y).toBe(11)
+  })
+  test('can be primed for movement without being moved', () => {
+    const device = new Device(100, 100, CLAMP_BOUNCE)
+    expect(device.x).toBe(100)
+    expect(device.y).toBe(100)
+    device.dx = 20
+    device.dy = 21
+    expect(device.x).toBe(100)
+    expect(device.y).toBe(100)
+  })
 })
