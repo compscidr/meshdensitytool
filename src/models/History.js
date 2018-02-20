132b247f85d36ff4b69ad3ba87fcc1d5c9d12d9b
Ben Hughes <benjamin.b.hughes@gmail.com>
2018-02-20 07:58:06 -0800

    Add startEntry to history class

--- src/models/History.js
+++ src/models/History.js
@@ -1,5 +1,18 @@
 class History {
+  constructor () {
+    this._entryInProgress = false
+  }
 
+  /**
+   * Add a new entry to the history.
+   */
+  startEntry () {
+    this._entryInProgress = true
+  }
+
+  get entryInProgress () {
+    return this._entryInProgress
+  }
 }
 
 export default History
--- src/models/History.test.js
+++ src/models/History.test.js
@@ -1,2 +1,14 @@
 import History from './History'
 
+describe('A history object', () => {
+  test('can be created', () => {
+    let hist = new History()
+  })
+
+  test('can have a new entry added', () => {
+    let hist = new History()
+    hist.startEntry()
+
+    expect(hist.entryInProgress).toBe(true)
+  })
+})
