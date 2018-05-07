113c901d474e0fe33532235144fdfb8990aa0cb1
Ben Hughes <benjamin.b.hughes@gmail.com>
2018-05-07 09:10:20 -0700

    MeshGrouper skeleton
    The meshgrouper's goal is to efficiently process graphs into disjoint
    subgraphs.

---
+++ src/models/graphtools/MeshGrouper.js
@@ -0,0 +1,8 @@
+/**
+ * The MeshGrouper handles analytics over a set of devices.
+ * It can tell you how many disjoint local meshes there are,
+ * and how big each one is.
+ */
+class MeshGrouper {
+  
+}
---
+++ src/models/graphtools/MeshGrouper.test.js
@@ -0,0 +1,5 @@
+describe('A MeshGrouper', () => {
+  test('can group a trivial set of nodes', () => {
+    expect(true).toBe(false)
+  })
+})
